85aff407514dbf417a7f4459ed83e794537c6aba
fmamadou <mmmfall73@gmail.com>
2024-10-13 11:56:23 +0000

    init

---
+++ src/main/java/com/fall3m/demoTestJenkins/DemoTestJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.fall3m.demoTestJenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTestJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoTestJenkinsApplication.class, args);
+	}
+
+}
